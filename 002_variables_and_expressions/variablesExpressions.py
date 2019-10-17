a0d0f4c8ba49e6cc09f61710766d183a350b17b3
Ricardo Escobar <ricardo.escobar@gmail.com>
2019-10-17 11:49:29 -0500

    Fixes ERROR: variables of different types cannot be combined with a str() cast function 002_variables_and_expressions/variablesExpressions.py

--- 002_variables_and_expressions/variablesExpressions.py
+++ 002_variables_and_expressions/variablesExpressions.py
@@ -9,6 +9,6 @@ variable = "abc"
 print(variable)
 
 # ERROR: variables of different types cannot be combined
-print("This is a string" + 123)
+print("This is a string " + str(123))
 
 # Global vs. local variables in functions
