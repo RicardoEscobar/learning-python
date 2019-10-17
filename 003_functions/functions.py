6e6c37c00bb9eec8c6069ea9ac2a0b3ef0fcf2c7
Ricardo Escobar <ricardo.escobar@gmail.com>
2019-10-17 13:27:36 -0500

    Add a basic function and test several print statements 003_functions/functions.py

--- 003_functions/functions.py
+++ 003_functions/functions.py
@@ -1,9 +1,15 @@
 # Example file for working with functions
 
 # define a basic function
+def function1():
+    print("I'm a function.")
 
 # function that takes arguments
 
 # function that returns a value
 
 # function with default value for an argument
+
+function1()
+print (function1())
+print(function1)
