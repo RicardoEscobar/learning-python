a4b0771530574578969d1dd9ebb6417df071063a
Ricardo Escobar <ricardo.escobar@gmail.com>
2019-10-17 12:03:39 -0500

    Global vs. local variables in functions 002_variables_and_expressions/variablesExpressions.py

--- 002_variables_and_expressions/variablesExpressions.py
+++ 002_variables_and_expressions/variablesExpressions.py
@@ -5,10 +5,16 @@ variable = 0
 print(variable)
 
 # re-declaring the variable works
-variable = "abc"
+variable = "global variable"
 print(variable)
 
 # ERROR: variables of different types cannot be combined
 print("This is a string " + str(123))
 
 # Global vs. local variables in functions
+def someFunction():
+    variable = "local variable"
+    print(variable)
+
+someFunction()
+print(variable)
