c99c25de4edba45555e9905a438233050f3223c3
Ricardo Escobar <ricardo.escobar@gmail.com>
2019-10-18 15:30:02 -0500

    use date comparison to see if April Fool's has already gone for this year 007_dates_and_time/timedeltas.py

--- 007_dates_and_time/timedeltas.py
+++ 007_dates_and_time/timedeltas.py
@@ -3,7 +3,7 @@
 from datetime import date, time, datetime, timedelta
 
 # constant to avoid magic number
-DAYS_IN_YEAR = 365
+DAYS_IN_YEAR = 36
 
 # construct a basic timedelta and print it
 print(timedelta(days = 365, hours = 5, minutes = 1))
@@ -26,12 +26,15 @@ time_ago_string = time_ago.strftime("%A %B %d, %Y")
 print("One week ago it was: " + time_ago_string)
 
 ### How many days until April Fools' Day?
-
+today = date.today()
+april_fools_day = date(today.year, 4, 1)
 
 # use date comparison to see if April Fool's has already gone for this year
 # if it has, use the replace() function to get the date for next year
+if april_fools_day < today:
+    print("April Fool's day already went by %d days ago" % ((today - april_fools_day).day))
+    april_fools_day = april_fools_day.replace(year = today.year + 1)
 
-
-# Now calculate the amount of time until April Fool's Day  
+# Now calculate the amount of time until April Fool's Day
 
 
