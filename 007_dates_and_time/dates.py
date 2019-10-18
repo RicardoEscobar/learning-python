20ea28ef2167fb230170b5ff1b33505b82a253e0
Ricardo Escobar <ricardo.escobar@gmail.com>
2019-10-18 12:41:46 -0500

    Retrieve today's weekday (0=Monday, 6=Sunday) 007_dates_and_time/dates.py

--- 007_dates_and_time/dates.py
+++ 007_dates_and_time/dates.py
@@ -8,11 +8,12 @@ def main():
     # Get today's date from the simple today() method from the date class
     today = date.today()
     print("Today's date is: ", today)
+
     # print out the date's individual components
+    print("Date componets: ", today.day, today.month, today.year)
 
-    
     # retrieve today's weekday (0=Monday, 6=Sunday)
-
+    print("Today's weekday # is: ", today.weekday())
     
     ## DATETIME OBJECTS
     # Get today's date from the datetime class
