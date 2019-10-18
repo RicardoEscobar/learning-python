35c9f1e421b62ae91020adc1de2ddec11959fddf
Ricardo Escobar <ricardo.escobar@gmail.com>
2019-10-18 09:50:34 -0500

    Makes the main() function executable if the file is executed ny itself 005_loops/loops.py

--- 005_loops/loops.py
+++ 005_loops/loops.py
@@ -4,9 +4,15 @@ def main():
     number_x = 0
 
     # define a while loop
+    while (number_x < 5):
+        print(number_x)
+        number_x = number_x + 1
 
     # define a for loop
 
     # use a for loop over a collection
 
     # use the break and continue statements
+
+if __name__ == "__main__":
+    main()
