471f5daa2920d962f5ed751929904cf9ffb42ce7
Ricardo Escobar <ricardo.escobar@gmail.com>
2019-10-18 13:01:10 -0500

    Prints the current year 007_dates_and_time/format.py

--- 007_dates_and_time/format.py
+++ 007_dates_and_time/format.py
@@ -3,20 +3,21 @@
 from datetime import datetime
 
 def main():
-  # Times and dates can be formatted using a set of predefined string
-  # control codes 
+    # Times and dates can be formatted using a set of predefined string
+    # control codes 
+    now = datetime.now()
 
-  #### Date Formatting ####
-  
-  # %y/%Y - Year, %a/%A - weekday, %b/%B - month, %d - day of month
+    #### Date Formatting ####
+    
+    # %y/%Y - Year, %a/%A - weekday, %b/%B - month, %d - day of month
+    print(now.strftime("The current year is: %Y"))
+
+    # %c - locale's date and time, %x - locale's date, %X - locale's time
 
 
-  # %c - locale's date and time, %x - locale's date, %X - locale's time
-
-
-  #### Time Formatting ####
-  
-  # %I/%H - 12/24 Hour, %M - minute, %S - second, %p - locale's AM/PM
+    #### Time Formatting ####
+    
+    # %I/%H - 12/24 Hour, %M - minute, %S - second, %p - locale's AM/PM
 
 
 
