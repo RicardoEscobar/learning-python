32c53bda57c13242c4a47b4fda25139a018ab99d
Ricardo Escobar <ricardo.escobar@gmail.com>
2019-10-18 09:47:13 -0500

    Adds 005_loops/loops.py

---
+++ 005_loops/loops.py
@@ -0,0 +1,12 @@
+# Example file for working with loops
+
+def main():
+    number_x = 0
+
+    # define a while loop
+
+    # define a for loop
+
+    # use a for loop over a collection
+
+    # use the break and continue statements
