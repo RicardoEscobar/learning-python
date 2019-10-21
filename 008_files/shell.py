0664f7ae5770447c557918ae45e8511c8b30b741
Ricardo Escobar <ricardo.escobar@gmail.com>
2019-10-21 12:38:19 -0500

    make a backup copy by appending "bak" to the name 008_files/shell.py

--- 008_files/shell.py
+++ 008_files/shell.py
@@ -6,20 +6,22 @@ import shutil
 
 
 def main():
-  # make a duplicate of an existing file
-  file_name = "textfile.txt"
+    # make a duplicate of an existing file
+    file_name = "text_file.txt"
+
     if path.exists(file_name):
         # get the path to the file in the current directory
         source = path.realpath(file_name)
 
         # let's make a backup copy by appending "bak" to the name
-
+        destiny = source + ".bak"
 
         # copy over the permissions, modification times, and other info
 
 
         # rename the original file
-
+        new_file_name = "new_text_file.txt"
+        os.rename(file_name, new_file_name)
 
         # now put things into a ZIP archive
 
