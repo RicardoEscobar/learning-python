ae6cc23ddf423856cac3ab6c6f6506204edf4ea7
Ricardo Escobar <ricardo.escobar@gmail.com>
2019-10-23 13:35:13 -0500

    Reads data from a server and prints the results 009_web_data/inet_data.py

--- 009_web_data/inet_data.py
+++ 009_web_data/inet_data.py
@@ -1,7 +1,11 @@
 # 
 # Example file for retrieving data from the internet
 #
+import urllib.request
 
+def main():
+    # open a connection to a URL using urllib2
+    web_url = urllib.request.urlopen("http://www.google.com")
 
 if __name__ == "__main__":
     main()
