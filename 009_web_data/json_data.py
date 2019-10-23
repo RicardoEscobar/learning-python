e3be9a06e8e77b260ab4c1f88bbc170bc80f6bfb
Ricardo Escobar <ricardo.escobar@gmail.com>
2019-10-23 13:53:18 -0500

    output the number of events, plus the magnitude and each event name  009_web_data/json_data.py

--- 009_web_data/json_data.py
+++ 009_web_data/json_data.py
@@ -9,10 +9,12 @@ def printResults(data):
   theJSON = json.loads(data)
   
   # now we can access the contents of the JSON like any other Python object
-
+  if "title" in theJSON["metadata"]:
+    print (theJSON["metadata"]["title"])
   
   # output the number of events, plus the magnitude and each event name  
-
+  count = theJSON["metadata"]["count"]
+  print (str(count) + " events recorded")
 
   # for each event, print the place where it occurred
 
@@ -28,11 +30,16 @@ def main():
   # In this case we'll use the free data feed from the USGS
   # This feed lists all earthquakes for the last day larger than Mag 2.5
   urlData = "http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_day.geojson"
-
+  
   # Open the URL and read the data
   webUrl = urllib.request.urlopen(urlData)
   print ("result code: " + str(webUrl.getcode()))
-
+  if (webUrl.getcode() == 200):
+    data = webUrl.read()
+    # print out our customized results
+    printResults(data)
+  else:
+    print ("Received an error from server, cannot retrieve results " + str(webUrl.getcode()))
 
 if __name__ == "__main__":
   main()
