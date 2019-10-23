444c16a2ba8fa7ec32737bd7101b354572254e32
Ricardo Escobar <ricardo.escobar@gmail.com>
2019-10-23 13:39:13 -0500

    read the data from the URL and print it 009_web_data/inet_data.py

--- 009_web_data/inet_data.py
+++ 009_web_data/inet_data.py
@@ -10,5 +10,9 @@ def main():
     # get the result code and print it
     print ("result code: " + str(web_url.getcode()))
 
+    # read the data from the URL and print it
+    data = web_url.read()
+    print (data)
+
 if __name__ == "__main__":
     main()
