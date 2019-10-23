83b78b150c63e3e8f961ab83c738eb0b48e70f9b
Ricardo Escobar <ricardo.escobar@gmail.com>
2019-10-23 13:36:34 -0500

    get the result code and print it 009_web_data/inet_data.py

--- 009_web_data/inet_data.py
+++ 009_web_data/inet_data.py
@@ -7,5 +7,8 @@ def main():
     # open a connection to a URL using urllib2
     web_url = urllib.request.urlopen("http://www.google.com")
 
+    # get the result code and print it
+    print ("result code: " + str(web_url.getcode()))
+
 if __name__ == "__main__":
     main()
